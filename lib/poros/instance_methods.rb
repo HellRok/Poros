1c34801aebf2fd096b56ea50d27ed720a3088fba
Sean Earle <sean@mindvision.com.au>
2018-04-29 14:43:08 +0930

    Move to simpler syntax for squishing

--- lib/poros/instance_methods.rb
+++ lib/poros/instance_methods.rb
@@ -1,25 +1,27 @@
 require 'poros/info'
 
-module Poros::InstanceMethods
-  attr_accessor :uuid
-  def uuid
-    @uuid ||= SecureRandom.uuid
-  end
+module Poros
+  module InstanceMethods
+    attr_accessor :uuid
+    def uuid
+      @uuid ||= SecureRandom.uuid
+    end
 
-  def poros
-    @poros ||= Poros::Info.new(self)
-  end
+    def poros
+      @poros ||= Poros::Info.new(self)
+    end
 
-  def destroy
-    File.delete(poros.file_path)
-    self.class.remove_from_index(self)
-    self
-  end
+    def destroy
+      File.delete(poros.file_path)
+      self.class.remove_from_index(self)
+      self
+    end
 
-  def save
-    File.write(poros.file_path, poros.to_h.to_yaml)
-    self.class.data_changed = true
-    self.class.update_index(self)
-    self
+    def save
+      File.write(poros.file_path, poros.to_h.to_yaml)
+      self.class.data_changed = true
+      self.class.update_index(self)
+      self
+    end
   end
 end
